ed714e575ecad3b99eb1748280fa2eaaeffdcb7d
Jeric June Logan <loganjeric25@gmail.com>
2021-08-24 16:29:29 +0800

    Update highschool.php

--- autocad/outputs/highschool.php
+++ autocad/outputs/highschool.php
@@ -55,16 +55,16 @@
                                     <p>High School Level <span><strong>Tutorial No. 1 </strong></span> </p>
                                     <p><strong>Title: </strong><span>CPU Fan</span> </p>
                                 </div>
-                                <p><span><strong>Created by:</strong></span> Cyril Audrey Relato</p>
+                                <p><span><strong>Created by:</strong></span> Rhea Ariola</p>
                                 <div class="slide-content1">
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1iMtgmERrjftvXfSLSHEV5d6OEQzDFJfS/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1EprCum2IyRZWZJXVxbRwhBLtwfgPPSc_/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href=""><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1iMtgmERrjftvXfSLSHEV5d6OEQzDFJfS/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="ppt-btn">
-                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1aUTqkrLvzy8KWpXz3UKjYrkMx9Z-zxjS/preview?slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1aUTqkrLvzy8KWpXz3UKjYrkMx9Z-zxjS/preview#slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
                                     </div> <!-- End ppt-btn -->
                                 </div>
                             </div>
@@ -83,13 +83,13 @@
                                 <p><span><strong>Created by:</strong></span> Rhea Ariola</p>
                                 <div class="slide-content1">
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1zkW_7PmOibWPBDGmGH38r116wXe7ULKW/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/13pj7C7u6ERwImP9iqvE9fHC6SzBLL1LG/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href=""><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1zkW_7PmOibWPBDGmGH38r116wXe7ULKW/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="ppt-btn">
-                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1XAY3TuoKm2ukDEoUAktsanDaeKdct-aF/preview?slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1XAY3TuoKm2ukDEoUAktsanDaeKdct-aF/preview#slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
                                     </div> <!-- End ppt-btn -->
                                 </div>
                             </div>
@@ -108,13 +108,13 @@
                                 <p><span><strong>Created by:</strong></span> Rhea Ariola</p>
                                 <div class="slide-content1">
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1MjMavItYSVBWR4N4VJQ3LEJM44H6fqcL/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/17zcBVstvGuwBTGW2Uw_XfRU-Dcvl4dP8/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href=""><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1MjMavItYSVBWR4N4VJQ3LEJM44H6fqcL/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="ppt-btn">
-                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/12lh9cQYTU-ACDN8jLr39gNHh-PQ-qlge/preview?slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/12lh9cQYTU-ACDN8jLr39gNHh-PQ-qlge/preview#slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
                                     </div> <!-- End ppt-btn -->
                                 </div>
                             </div>
@@ -138,13 +138,13 @@
                                 <p><span><strong>Created by:</strong></span> Rhea Ariola</p>
                                 <div class="slide-content1">
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1H62o3dRp72-PO7KdmonUZ5KZSb0AqoD7/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1odc7a3-j4eBvGI5rtfxKqiwRkZYDiU2B/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href=""><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1H62o3dRp72-PO7KdmonUZ5KZSb0AqoD7/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="ppt-btn">
-                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1F5vZLtsQUi_gJtsqittoqQXlsSrmtyn9/preview?slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1F5vZLtsQUi_gJtsqittoqQXlsSrmtyn9/preview#slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
                                     </div> <!-- End ppt-btn -->
                                 </div>
                             </div>
@@ -158,18 +158,18 @@
                             <div class="clients-text">
                                 <div class="client-rating">
                                     <p>High School Level <span><strong>Tutorial No. 5 </strong></span> </p>
-                                    <p><strong>Title: </strong><span>PCle Inductor</span> </p>
+                                    <p><strong>Title: </strong><span>PCle Socket</span> </p>
                                 </div>
                                 <p><span><strong>Created by:</strong></span> Rhea Ariola</p>
                                 <div class="slide-content1">
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1kXzIEspGqTZyGCxEINykmS-hxt6_2N1u/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1fr9mnGHlSdCt5Q_yFztyVIgHtjXV-zPN/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="pdf-btn">
-                                        <a class="video-play vid-zone" href=""><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1kXzIEspGqTZyGCxEINykmS-hxt6_2N1u/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
                                     </div> <!-- End pdf-btn -->
                                     <div class="ppt-btn">
-                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/14jf3GTfCYXr8xQ_V7rueEUoVYwGfu-kO/preview?slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/14jf3GTfCYXr8xQ_V7rueEUoVYwGfu-kO/preview#slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
                                     </div> <!-- End ppt-btn -->
                                 </div>
                             </div>
