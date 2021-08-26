5d8d68cd9cd1baa66307168bc0846ca9135b9269
Jeric June Logan <loganjeric25@gmail.com>
2021-08-26 14:46:20 +0800

    added autocad lrt and mrt output pages

---
+++ autocad/outputs/lrt.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <?php include '../../components/head.inc.php'; ?>
+    <title>AutoCAD LRT Station Tutorial Outputs | 3D Modeling & Design</title>
+</head>
+<?php include '../../components/header.inc.php'; ?>
+<!-- start -->
+<!-- Start breadcumb Area -->
+<div class="page-area">
+    <div class="breadcumb-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="breadcrumb text-center">
+                    <div class="section-headline white-headline">
+                        <h3 class="title5">Outputs: <span>AutoCAD LRT Station Tutorial Outputs</span></h3>
+                    </div>
+                    <ul class="breadcrumb-bg">
+                        <a href="index.php">
+                            <li class="home-bread">Home</li>
+                        </a>
+                        <li class="home-bread">Outputs</li>
+                        <a href="../output.php">
+                            <li class="home-bread">AutoCAD</li>
+                        </a>
+                        <a href="#">
+                            <li>LRT Station Tutorials Outputs</li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End breadcumb Area -->
+<!-- Start Area -->
+<div class="reviews-area review-page-area page-padding-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="title9 text-center">
+                    <span>3D Design Software: AutoCAD LRT Station Tutorial Outputs</span>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="reviews-content ">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="single-testi">
+                        <div class="testi-text">
+                            <div class="clients-text">
+                                <div class="client-rating">
+                                    <p>LRT Station <span><strong>Tutorial No. 1 </strong></span> </p>
+                                    <p><strong>Title: </strong><span>Baclaran Station</span> </p>
+                                </div>
+                                <p><span><strong>Created by:</strong></span> Argie Dalos</p>
+                                <div class="slide-content1">
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1RHz3mnwFpPtGvACOC13H3Tq7ZmR7BQi_/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1580S1-8XQTsWRweXG7VoqsbQN69LYMZo/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="ppt-btn">
+                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1IxNPi5j0YhIxpTrNnEkB5UGy6fDC4zVB/preview#slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                    </div> <!-- End ppt-btn -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End single item -->
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="single-testi">
+                        <div class="testi-text">
+                            <div class="clients-text">
+                                <div class="client-rating">
+                                    <p>LRT Station Level <span><strong>Tutorial No. 2 </strong></span> </p>
+                                    <p><strong>Title: </strong><span>Buendia Station</span> </p>
+                                </div>
+                                <p><span><strong>Created by:</strong></span> Joyce Delos Reyes</p>
+                                <div class="slide-content1">
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1YWx7PnTBMZZRL1yZhgPX00YrvDhWWjKp/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1snMWIrAm1Gg7DO7oK4U8-YmcCu5Atrwh/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="ppt-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1HYTVnLya60EgluF5DZEuvWHMmDVvtPZq/preview"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                    </div> <!-- End ppt-btn -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End single item -->
+
+                <!-- End single item -->
+            </div> <!-- End reviews-content -->
+        </div> <!-- End row -->
+        <!-- End -->
+    </div>
+</div>
+<!-- end -->
+<?php
+include '../../components/footer.inc.php';
+include '../../components/js.inc.php';
+?>
+
+</html>
---
+++ autocad/outputs/mrt.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <?php include '../../components/head.inc.php'; ?>
+    <title>AutoCAD MRT Station Tutorial Outputs | 3D Modeling & Design</title>
+</head>
+<?php include '../../components/header.inc.php'; ?>
+<!-- start -->
+<!-- Start breadcumb Area -->
+<div class="page-area">
+    <div class="breadcumb-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="breadcrumb text-center">
+                    <div class="section-headline white-headline">
+                        <h3 class="title5">Outputs: <span>AutoCAD MRT Station Tutorial Outputs</span></h3>
+                    </div>
+                    <ul class="breadcrumb-bg">
+                        <a href="index.php">
+                            <li class="home-bread">Home</li>
+                        </a>
+                        <li class="home-bread">Outputs</li>
+                        <a href="../output.php">
+                            <li class="home-bread">AutoCAD</li>
+                        </a>
+                        <a href="#">
+                            <li>MRT Station Tutorials Outputs</li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End breadcumb Area -->
+<!-- Start Area -->
+<div class="reviews-area review-page-area page-padding-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="title9 text-center">
+                    <span>3D Design Software: AutoCAD MRT Station Tutorial Outputs</span>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="reviews-content ">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="single-testi">
+                        <div class="testi-text">
+                            <div class="clients-text">
+                                <div class="client-rating">
+                                    <p>MRT Station <span><strong>Tutorial No. 1 </strong></span> </p>
+                                    <p><strong>Title: </strong><span>3D MRT Station</span> </p>
+                                </div>
+                                <p><span><strong>Created by:</strong></span> Joy Ann Reyes</p>
+                                <div class="slide-content1">
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1i6vugODwfr4kqks-LPTFEzS7jHoUNpdr/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1yz005Vxr09Ir3ACA-vnpzm4ewJSkKFvA/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="ppt-btn">
+                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1ESD5MkcwZjpYYQgRsgNhJzqiZEq7gMes/preview#slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                    </div> <!-- End ppt-btn -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End single item -->
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="single-testi">
+                        <div class="testi-text">
+                            <div class="clients-text">
+                                <div class="client-rating">
+                                    <p>MRT Station Level <span><strong>Tutorial No. 2 </strong></span> </p>
+                                    <p><strong>Title: </strong><span>Buendia Station</span> </p>
+                                </div>
+                                <p><span><strong>Created by:</strong></span> Karl Mirro Divino</p>
+                                <div class="slide-content1">
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1wPrcLXJ5oVFlwPBSFLwocGzveQ5F1bax/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1epLcymKPAj5hlJqS6o4mZSIupyfcFNX0/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="ppt-btn">
+                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1C7cF7XBybqfrfbBbDNC5fzwLxuIEul2R/preview#slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                    </div> <!-- End ppt-btn -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End single item -->
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="single-testi">
+                        <div class="testi-text">
+                            <div class="clients-text">
+                                <div class="client-rating">
+                                    <p>MRT Station Level <span><strong>Tutorial No. 3 </strong></span> </p>
+                                    <p><strong>Title: </strong><span>Guadalupe Station</span> </p>
+                                </div>
+                                <p><span><strong>Created by:</strong></span> Erika Gwynette Santos</p>
+                                <div class="slide-content1">
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/16zfhopKMfj_IEv2RpsmaaFrwEAvmc8vK/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Instructional</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="pdf-btn">
+                                        <a class="video-play vid-zone" href="https://drive.google.com/file/d/1vMbNNp4f5PTzWkfAp86_DixDEGqzU7Rc/preview"><img class="pdf-ppt-icon" src=/img/logo/pdf-icon.png> PDF Creative</a>
+                                    </div> <!-- End pdf-btn -->
+                                    <div class="ppt-btn">
+                                        <a class="video-play vid-zone" href="https://docs.google.com/presentation/d/1K5gWO6A2xbIq8N_-9FnH6tKJPmQIh5xF/preview#slide=id.p1"><img class="pdf-ppt-icon" src=/img/logo/ppt-icon.png> PowerPoint</a>
+                                    </div> <!-- End ppt-btn -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End single item -->
+            </div> <!-- End reviews-content -->
+        </div> <!-- End row -->
+        <!-- End -->
+    </div>
+</div>
+<!-- end -->
+<?php
+include '../../components/footer.inc.php';
+include '../../components/js.inc.php';
+?>
+
+</html>
